ca8d07ec24a3b3f01c46d6cfc7bb1b090510751a
Liubov Hromadska <gromluba2@gmail.com>
2023-04-03 11:08:58 +0400

    homework 5

---
+++ homework5/task 1/script.js
@@ -0,0 +1,10 @@
+'use strict'
+//------------task 1-----------
+const elementWitId = document.getElementById("headerTwo");
+const elementWitTag = document.querySelector("section > h2");
+const elementWithNth = document.querySelector("li:nth-child(5)");
+const elementWithClass = document.getElementsByClassName("hatredLevelBlock");
+console.log(elementWitId);
+console.log(elementWitTag);
+console.log(elementWithNth);
+console.log(elementWithClass);
